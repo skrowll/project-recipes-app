1377175db2976bc4cf96c479a51ac5b6d7a5772b
Lucas Lopes <lucascaldaslopes15@gmail.com>
2022-04-26 16:56:50 -0300

    correção req 7

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 
 const PASSWORD_MAX_LENGTH = 6;
 
 export default function Login() {
+  const history = useHistory();
+
   const [state, setState] = useState({
     email: '',
     password: '',
@@ -15,6 +17,7 @@ export default function Login() {
     localStorage.setItem('mealsToken', 1);
     localStorage.setItem('cocktailsToken', 1);
     localStorage.setItem('user', JSON.stringify({ email: state.email }));
+    history.push('/foods');
   };
 
   const handleInputChange = ({ target: { name, value } }) => {
@@ -48,15 +51,14 @@ export default function Login() {
         value={ state.password }
         onChange={ handleInputChange }
       />
-      <Link to="/foods">
-        <button
-          disabled={ buttonDisabled }
-          data-testid="login-submit-btn"
-          type="submit"
-        >
-          Entrar
-        </button>
-      </Link>
+      <button
+        disabled={ buttonDisabled }
+        data-testid="login-submit-btn"
+        type="submit"
+        onClick={ saveToken }
+      >
+        Entrar
+      </button>
     </form>
   );
 }
