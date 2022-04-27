191ab1fba88f1e026fa0ba46c229e6fdb5f36ceb
Raphael <rafa.ramos_22@hotmail.com>
2022-04-26 22:26:02 -0300

    req 13 e 14

--- src/Components/SearchBar.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function SearchBar() {
-  return (
-    <form>
-      <input data-testid="search-input" type="text" />
-    </form>
-  );
-}
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,77 @@
+import React, { useState } from 'react';
+import request from '../services/services';
+
+export default function SearchBar() {
+  const [endPoint, setEndPonit] = useState('');
+
+  const onChange = ({ target: { value } }) => {
+    console.log(value);
+    setEndPonit((prevState) => ({
+      ...prevState,
+      value,
+    }));
+  };
+
+  const searchClick = () => {
+    const element = document.querySelector('input[name=search]:checked').value;
+
+    if (element === 'ingredient') {
+      return `https://www.themealdb.com/api/json/v1/1/filter.php?i=${endPoint.value}`;
+    }
+    if (element === 'name') {
+      return `https://www.themealdb.com/api/json/v1/1/search.php?s=${endPoint.value}`;
+    }
+    if (element === 'first-letter' && endPoint.value.length === 1) {
+      return `https://www.themealdb.com/api/json/v1/1/search.php?f=${endPoint.value}`;
+    }
+    global.alert('Your search must have only 1 (one) character');
+  };
+  return (
+    <form>
+      <input
+        data-testid="search-input"
+        type="text"
+        onChange={ onChange }
+
+      />
+      <label htmlFor="ingredient">
+        <input
+          type="radio"
+          data-testid="ingredient-search-radio"
+          id="ingredient"
+          name="search"
+          value="ingredient"
+        />
+        Ingredient
+      </label>
+      <label htmlFor="name">
+        <input
+          type="radio"
+          data-testid="name-search-radio"
+          id="name"
+          name="search"
+          value="name"
+        />
+        Name
+      </label>
+      <label htmlFor="first-letter">
+        <input
+          type="radio"
+          data-testid="first-letter-search-radio"
+          id="first-letter"
+          name="search"
+          value="first-letter"
+        />
+        First letter
+      </label>
+      <button
+        type="button"
+        data-testid="exec-search-btn"
+        onClick={ () => request(searchClick()) }
+      >
+        buscar
+
+      </button>
+    </form>
+  );
+}
