d6db95c212373a8e1e77637b09ed61f9a9934d66
skrowll <lucasdejesus.rocha@hotmail.com>
2023-04-03 17:39:08 -0300

    feat: import Header and css, and add classes

--- src/pages/FoodRecipeInProgress.jsx
+++ src/pages/FoodRecipeInProgress.jsx
@@ -6,9 +6,11 @@ import { containsIngredient, isFavorite, removeFavorite, removeInProgressIngredi
   request, saveFavorite, saveInProgressIngredient,
   saveFinish } from '../services/services';
 import { recipeDetailsEndpoint } from './FoodRecipe';
+import Header from '../components/Header';
 import shareIcon from '../images/shareIcon.svg';
 import whiteHeart from '../images/whiteHeartIcon.svg';
 import blackHeart from '../images/blackHeartIcon.svg';
+import '../styles/pages/RecipeInProgress.css';
 
 export default function FoodRecipeInProgress({ match: { params: { id } } }) {
   const [recipe, setRecipe] = useState({});
@@ -87,79 +89,88 @@ export default function FoodRecipeInProgress({ match: { params: { id } } }) {
 
   return (
     <div>
-      <img
-        data-testid="recipe-photo"
-        style={ { width: '400px' } }
-        src={ recipe.strMealThumb }
-        alt={ recipe.strMeal }
-      />
-      {linkCopied && <p>Link copied!</p>}
-      <h3 data-testid="recipe-title">{ recipe.strMeal }</h3>
-      <button
-        data-testid="share-btn"
-        type="button"
-        onClick={ copy }
-      >
-        <img src={ shareIcon } alt="Share" />
+      <Header title="Recipe in progress" />
+      <div className="recipeInProgressContainer">
+        <div className="imageContainer">
+          <img
+            data-testid="recipe-photo"
+            className="recipeImage"
+            // style={ { width: '400px' } }
+            src={ recipe.strMealThumb }
+            alt={ recipe.strMeal }
+          />
+        </div>
+        {linkCopied && <p>Link copied!</p>}
+        <h3 className="recipeName" data-testid="recipe-title">{ recipe.strMeal }</h3>
+        <p data-testid="recipe-category">{ `Category: ${recipe.strCategory}` }</p>
+        <div className="socialContainer">
+          <button
+            data-testid="share-btn"
+            type="button"
+            onClick={ copy }
+          >
+            <img src={ shareIcon } alt="Share" />
 
-      </button>
-      <button
-        type="button"
-        onClick={ handleFavoriteClick }
-      >
-        <img
-          data-testid="favorite-btn"
-          src={ favorite ? blackHeart : whiteHeart }
-          alt="whiteHeart"
-        />
-      </button>
-      <p data-testid="recipe-category">{recipe.strCategory}</p>
-      <ul>
-        {
-          getIngredients().map((ingredient, index) => (
-            <li
-              key={ ingredient }
-              data-testid={ `${index}-ingredient-step` }
-            >
-              <label htmlFor={ `${index}-ingedient-step` }>
-                <input
-                  id={ `${index}-ingedient-step` }
-                  type="checkbox"
-                  value={ ingredient }
-                  checked={ containsIngredient('meals', id, ingredient) }
-                  onChange={ ingredientChange }
-                  onClick={ handleFinish }
-                />
-                &nbsp;
-                {`${ingredient} 
-                  ${getMeasures()[index] ? `- ${getMeasures()[index]}` : ''}`}
-              </label>
-            </li>))
-        }
-      </ul>
-      <br />
-      <p data-testid="instructions">{recipe.strInstructions}</p>
-      <br />
-      <Link to="/done-recipes">
-        <button
-          data-testid="finish-recipe-btn"
-          type="button"
-          disabled={ disabled }
-          onClick={
-            () => saveFinish({
-              id: recipe.idMeal,
-              type: 'food',
-              nationality: recipe.strArea,
-              category: recipe.strCategory,
-              alcoholicOrNot: '',
-              name: recipe.strMeal,
-              image: recipe.strMealThumb,
-            })
+          </button>
+          <button
+            type="button"
+            onClick={ handleFavoriteClick }
+          >
+            <img
+              data-testid="favorite-btn"
+              src={ favorite ? blackHeart : whiteHeart }
+              alt="whiteHeart"
+            />
+          </button>
+        </div>
+        <ul>
+          {
+            getIngredients().map((ingredient, index) => (
+              <li
+                key={ ingredient }
+                data-testid={ `${index}-ingredient-step` }
+              >
+                <label htmlFor={ `${index}-ingedient-step` }>
+                  <input
+                    id={ `${index}-ingedient-step` }
+                    type="checkbox"
+                    value={ ingredient }
+                    checked={ containsIngredient('meals', id, ingredient) }
+                    onChange={ ingredientChange }
+                    onClick={ handleFinish }
+                  />
+                  &nbsp;
+                  {`${ingredient} 
+                    ${getMeasures()[index] ? `- ${getMeasures()[index]}` : ''}`}
+                </label>
+              </li>))
           }
-        >
-          Finish
-        </button>
-      </Link>
+        </ul>
+        <br />
+        <p data-testid="instructions">{recipe.strInstructions}</p>
+        <br />
+        <Link to="/done-recipes">
+          <button
+            data-testid="finish-recipe-btn"
+            type="button"
+            className="finishButton"
+            disabled={ disabled }
+            onClick={
+              () => saveFinish({
+                id: recipe.idMeal,
+                type: 'food',
+                nationality: recipe.strArea,
+                category: recipe.strCategory,
+                alcoholicOrNot: '',
+                name: recipe.strMeal,
+                image: recipe.strMealThumb,
+              })
+            }
+          >
+            Finish
+          </button>
+        </Link>
+      </div>
     </div>
   );
 }
